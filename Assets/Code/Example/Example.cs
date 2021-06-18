5168be62afcd48261952988ac7592342124b9072
David Pino <aesthezel@gmail.com>
2021-06-18 19:02:46 -0400

    Boilerplate of Unity project

---
+++ Assets/Code/Example/Example.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// API
+public class Example : MonoBehaviour
+{
+    #region [MonoBehaviour API]
+
+    void Start()
+    {
+
+    }
+
+    // Es decir que esto se ejecuta 1 vez cada fotograma y mientras este el objeto activo en escena 
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
